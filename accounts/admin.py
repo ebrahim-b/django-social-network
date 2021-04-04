b85887670bff6179645157cfb327e69471ae4b75
Ebrahim Barati Nasab <abrahim_barati@yahoo.com>
2021-04-04 18:13:32 +0430

    Add Profile & FollowModels

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Profile, Follow
 
 admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Follow)
--- accounts/models.py
+++ accounts/models.py
@@ -26,8 +26,6 @@ class UserManager(BaseUserManager):
         return self.create_user(phone, password, **other_fields)
 
 class User(AbstractUser):
-    # r'^[0][9]\d{9}$|^[1-9]\d{9}$'
-    # r'^\+?1?\d{11}$'
     username = None
     phone_regex = RegexValidator(regex = r'^[0][9]\d{9}$|^[1-9]\d{9}$',
             message = 'Phone number up to 11 digits allowed start with 09')
@@ -43,3 +41,49 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     backend = 'accounts.custombackend.PhoneBackend'
+
+def file_path_dir(instance, filename):
+    return "uploaded/user/" + str(instance.user_name) + "/profile_pic/" + filename
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_name = models.CharField(max_length=30, blank=False, unique=True)
+
+    MALE = 1
+    FEMALE = 2
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+    )
+    gender = models.IntegerField(choices=GENDER_CHOICES, null=True, blank=True)
+
+    bio = models.TextField(max_length=300, null=True, blank=True)
+    profile_image = models.ImageField(default='profile_pics/default.png', upload_to=file_path_dir)
+    birthdate = models.DateTimeField(blank=True, null=True)
+    created_date = models.DateTimeField(blank=True, default=timezone.now)
+
+    PAID = 1
+    TRIAL = 2
+    ACCOUNT_TYPE_CHOICES = (
+        (PAID, 'Paid'),
+        (TRIAL, 'Trial'),
+    )
+    account_type = models.IntegerField(choices=ACCOUNT_TYPE_CHOICES, default=TRIAL)
+
+    def __str__(self):
+        return f'{self.user_name}'
+
+    @property
+    def followers(self):
+        return Follow.objects.filter(follow_user=self.user).count()
+
+    @property
+    def following(self):
+        return Follow.objects.filter(user=self.user).count()
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+    follow_user = models.ForeignKey(User, related_name='follow_user', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
